9256a8287fe0a615ac079c8aaf57d1c57a33106f
Nick <id90820077@yandex.ru>
2020-05-08 23:23:02 +0300

    game for x/o

--- scripts/script.js
+++ scripts/script.js
@@ -1,49 +1,53 @@
-let x = "X";
-let o = "O";
+let xoSettings = {
+	width: 3,
+	height: 3,
+	playerSide: "X",
+	botSide: "O",
+}
+
+let cells;
 let table = document.querySelector(".xo-field");
-let cells = document.querySelectorAll(".xo-cell");
 table.addEventListener("click", playGame);
 document.getElementById("first").addEventListener("click", choice);
 document.getElementById("second").addEventListener("click", choice);
 
 
 function playGame(event) {
-	let str;
 	let target = event.target;
-	pen(target, x);
+	if (target.className != "xo-cell" || target.innerHTML != "") return;
+	pen(target);
 
 	draw();
 
-	if (isOver(x) == x) result.innerHTML = "Победа";
+	if (isOver(xoSettings.playerSide) == xoSettings.playerSide) result.innerHTML = "Победа";
 
-	pen(target, o);
+	penBot();
 
-	if (isOver(o) == o) result.innerHTML = "Поражение";
-	if (result.innerHTML != "") table.removeEventListener("click", playGame);
+	if (isOver(xoSettings.botSide) == xoSettings.botSide) result.innerHTML = "Поражение";
+	if (result.innerHTML == "") draw();
+	if (result.innerHTML != "") {
+		table.removeEventListener("click", playGame); 
+		newGame();
+	}
 }
 
 function randomInt(max) {
 	return Math.floor(Math.random() * max);
 }
 
-function pen(target, str) {
-
-	if (str == x) {
-		if (target.className != "xo-cell" || target.innerHTML != "") return;
-		target.innerHTML = x;
-	}
-	penO(str);
+function pen(target) {
+	target.innerHTML = xoSettings.playerSide;
 }
 
-function penO(str) {
-	if (result.innerHTML == "" && str == o) {
+function penBot() {
+	if (result.innerHTML == "") {
 		while (true) {
 			let x = randomInt(3);
 			let y = randomInt(3);
 			let newId = x + "_" + y;
 
 			if (document.getElementById(newId).innerHTML != "") continue;
-			document.getElementById(newId).innerHTML = o;
+			document.getElementById(newId).innerHTML = xoSettings.botSide;
 			break;
 		}
 	}
@@ -57,7 +61,7 @@ function isOver(str) {
 			&& document.getElementById("1_1").innerHTML == str
 			&& document.getElementById("2_0").innerHTML == str)) return str;
 
-	for (let i = 0; i < 3; i++) {
+	for (let i = 0; i < xoSettings.width; i++) {
 		if ((document.getElementById(i + "_0").innerHTML == str
 			&& document.getElementById(i + "_1").innerHTML == str
 			&& document.getElementById(i + "_2").innerHTML == str)
@@ -72,22 +76,54 @@ function draw() {
 	for (let cell of cells) {
 		if (cell.innerHTML != "") count++;
 	}
+	console.log(count);
 	if (count == 9) result.innerHTML = "Ничья!";
 }
 
 function choice(event) {
 	let field = document.querySelector(".xo-field");
+	let downField = document.createElement("div");
+	downField.id = "deleteField";
+	field.append(downField);
+
 	field.style.visibility = "visible";
 
-	for (let i = 0; i < 3; i++) {
-		for (let j = 0; j < 3 ; j++){
+	for (let i = 0; i < xoSettings.height; i++) {
+		for (let j = 0; j < xoSettings.width; j++) {
 			let elem = document.createElement("DIV");
 			elem.className = "xo-cell";
-			elem.id = i + "_" + j; 
-			field.append(elem);
+			elem.id = i + "_" + j;
+			downField.append(elem);
 		}
-		field.append(document.createElement("br"));
+		downField.append(document.createElement("br"));
 	}
+	cells = document.querySelectorAll(".xo-cell");
+
 	document.querySelector(".menu").style.visibility = "hidden";
-	
+
+	let target = event.target;
+	if (target.id == "second") {
+		xoSettings.playerSide = "O";
+		xoSettings.botSide = "X";
+		penBot(xoSettings.botSide);
+	}
+	else if (target.id == "first") {
+		xoSettings.playerSide = "X";
+		xoSettings.botSide = "O";
+	}
+	//	let form = document.querySelector(".menu");
+}
+
+function newGame(){
+	let input = document.createElement("input");
+	input.setAttribute("type", "button");
+	input.setAttribute( "value","Начать новую игру");
+	input.className = "xo-newGame";
+	document.querySelector(".xo-field").append(input);
+
+	document.querySelector(".menu").style.visibility = "visible";
+
+	result.innerHTML = "";
+	deleteField.remove();
+	document.querySelector(".xo-newGame").remove();
 }
